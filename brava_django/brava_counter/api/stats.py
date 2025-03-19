312fb197aceaf8a6e0e72ddf396eddba645dcf79
Ng <doratheexplorerlovesdeigo@gmail.com>
2025-03-19 04:02:19 -0400

    Update stats to get today's data

--- brava_django/brava_counter/api/stats.py
+++ brava_django/brava_counter/api/stats.py
@@ -6,7 +6,8 @@ from .models import MoveCounter
 def calculate_daily_sum():
    # today = datetime.now().date()
    today = now().date()
-   tomorrow = today + timedelta(1)
+   tomorrow1 = today + timedelta(1)
+   pastWeek = today - timedelta(6)
 
    # today_start = datetime.combine(today, time(0, 0, 0))  # Midnight today
    # today_end = datetime.combine(tomorrow, time(0, 0, 0))  # Midnight tomorrow
@@ -14,7 +15,7 @@ def calculate_daily_sum():
    # return MoveCounter.objects.filter(updated_at = start__lte=today_end, end__lte=today_start).aggregate(
    # return MoveCounter.objects.filter(updated_at__range=(today_start, today_end)).aggregate(
    # return MoveCounter.objects.filter().aggregate(
-   return MoveCounter.objects.filter(updated_at__date=tomorrow).aggregate(
+   return MoveCounter.objects.filter(updated_at__date=today).aggregate(
       DLJ_daily = Sum('DLJ_counter'),
       DLR_daily = Sum('DLR_counter'),
       SLJ_L_daily = Sum('SLJ_L_counter'),  
